a4481dcb0f0b942886adcde071a9b0b4f41a286e
DDaarcon <dda.darek@gmail.com>
2021-04-09 13:03:56 +0200

    ads counter fix

--- Assets/Scripts/SaveGameDataState.cs
+++ Assets/Scripts/SaveGameDataState.cs
@@ -7,6 +7,7 @@ public class SaveGameDataState
 		public bool unlocked {get; private set;}
 
 		public LevelUnlock(string unlockKey) {
+			this.unlockKey = unlockKey;
 			if (PlayerPrefs.HasKey(unlockKey)) {
 				unlocked = PlayerPrefs.GetInt(unlockKey) == 1;
 			} else {
@@ -25,6 +26,7 @@ public class SaveGameDataState
 		public int adsLeft {get; private set;}
 
 		public AdsUnlock(string unlockKey, string adsLeftKey, int adsOnStart) : base(unlockKey) {
+			this.adsLeftKey = adsLeftKey;
 			if (PlayerPrefs.HasKey(adsLeftKey)) {
 				adsLeft = PlayerPrefs.GetInt(adsLeftKey);
 			} else {
@@ -52,29 +54,12 @@ public class SaveGameDataState
 
 
 	public LevelUnlock goldTheme = new LevelUnlock("GOLD_TH");
-    // public bool goldThemeUnlocked;
-	// public bool goldThemePostTry;
 	public LevelUnlock minecrafTheme = new LevelUnlock("MINECRAFT_TH");
-    // public bool minecraftThemeUnlocked;
-	// public bool minecraftThemePostTry;
 	public LevelUnlock redShinyTheme = new LevelUnlock("REDSHINY_TH");
-	// public bool redShinyThemeUnlocked;
-	// public bool redShinyThemePostTry;
 	public LevelUnlock copperTheme = new LevelUnlock("COPPER_TH");
-	// public bool copperThemeUnlocked;
-	// public bool copperThemePostTry;
 	public AdsUnlock blueShinyTheme = new AdsUnlock("BLUESHINY_TH", "BLUESHINY_ADS", 10);
-	// public bool blueShinyThemeUnlocked;
-	// public bool blueShinyThemePostTry;
-	// public int blueShinyAdsLeft;
 	public AdsUnlock darkElementTheme = new AdsUnlock("DARKELEMENT_TH", "DARKELEMENT_ADS", 15);
-	// public bool darkElementThemeUnlocked;
-	// public bool darkElementThemePostTry;
-	// public int darkElementAdsLeft;
 	public AdsUnlock ticTacToeTheme = new AdsUnlock("TICTACTOE_TH", "TICTACTOE_ADS", 20);
-	// public bool ticTacToeThemeUnlocked;
-	// public bool ticTacToeThemePostTry;
-	// public int ticTacToeAdsLeft;
 
 	public SaveGameDataState() {
 		if (PlayerPrefs.HasKey(SET_THEME_KEY)) {
@@ -83,63 +68,4 @@ public class SaveGameDataState
 			SetTheme = Themes.BasicStone;
 		}
 	}
-
-
-	// public void SetPostTry(Themes themes, bool on) {
-	// 	switch (themes) {
-	// 		case Themes.BasicStone:
-	// 		default:
-	// 			break;
-	// 		case Themes.Gold:
-	// 			goldThemePostTry = on;
-	// 			break;
-	// 		case Themes.Minecraft:
-	// 			minecraftThemePostTry = on;
-	// 			break;
-	// 		case Themes.RedShiny:
-	// 			redShinyThemePostTry = on;
-	// 			break;
-	// 		case Themes.Copper:
-	// 			copperThemePostTry = on;
-	// 			break;
-	// 		case Themes.BlueShiny:
-	// 			blueShinyThemePostTry = on;
-	// 			break;
-	// 		case Themes.DarkElement:
-	// 			darkElementThemePostTry = on;
-	// 			break;
-	// 		case Themes.TicTacToe:
-	// 			ticTacToeThemePostTry = on;
-	// 			break;
-	// 	}
-	// }
-	// public void SetUnlocked(Themes themes, bool on) {
-	// 	switch (themes) {
-	// 		case Themes.BasicStone:
-	// 		default:
-	// 			break;
-	// 		case Themes.Gold:
-	// 			goldThemeUnlocked = on;
-	// 			break;
-	// 		case Themes.Minecraft:
-	// 			minecraftThemeUnlocked = on;
-	// 			break;
-	// 		case Themes.RedShiny:
-	// 			redShinyThemeUnlocked = on;
-	// 			break;
-	// 		case Themes.Copper:
-	// 			copperThemeUnlocked = on;
-	// 			break;
-	// 		case Themes.BlueShiny:
-	// 			blueShinyThemeUnlocked = on;
-	// 			break;
-	// 		case Themes.DarkElement:
-	// 			darkElementThemeUnlocked = on;
-	// 			break;
-	// 		case Themes.TicTacToe:
-	// 			ticTacToeThemeUnlocked = on;
-	// 			break;
-	// 	}
-	// }
-
 }
